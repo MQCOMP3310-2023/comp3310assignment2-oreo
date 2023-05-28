9a5ca28f687cd91f08ff0dfadd46da5ee706bb0f
bufOvf <84895938+Muhammad-Anderson@users.noreply.github.com>
2023-05-28 21:42:15 +1000

    Prevent SQL Injection in json.py
    Fix potential SQL injection vulnerabilities in restaurantMenuJSON and menuItemJSON methods. The change includes using parameterized queries and enforcing integer route parameters in Flask.

--- project/json.py
+++ project/json.py
@@ -7,16 +7,16 @@ import json as pyjs
 json = Blueprint('json', __name__)
 
 #JSON APIs to view Restaurant Information
-@json.route('/restaurant/<restaurant_id>/menu/JSON')
+@json.route('/restaurant/<int:restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
-    items = db.session.execute(text('select * from menu_item where restaurant_id = ' + str(restaurant_id)))
+    items = db.session.execute(text('select * from menu_item where restaurant_id = :restaurant_id'), {'restaurant_id': restaurant_id})
     items_list = [ i._asdict() for i in items ]
     return pyjs.dumps(items_list)
 
 
-@json.route('/restaurant/<restaurant_id>/menu/<int:menu_id>/JSON')
+@json.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
-    menu_item = db.session.execute(text('select * from menu_item where id = ' + str(menu_id) + ' limit 1'))
+    menu_item = db.session.execute(text('select * from menu_item where id = :menu_id limit 1'), {'menu_id': menu_id})
     items_list = [ i._asdict() for i in menu_item ]
     return pyjs.dumps(items_list)
 
@@ -25,5 +25,3 @@ def restaurantsJSON():
     restaurants = db.session.execute(text('select * from restaurant'))
     rest_list = [ r._asdict() for r in restaurants ]
     return pyjs.dumps(rest_list)
-
-

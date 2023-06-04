0afe17a086764c74128c3a65aa427210d447c4b9
Thuseevan1 <88169000+Thuseevan1@users.noreply.github.com>
2023-06-04 17:14:06 +1000

    removed unused code

--- project/json.py
+++ project/json.py
@@ -45,35 +45,3 @@ def restaurants_json():
     restaurants = db.session.execute(query)
     rest_list = [r._asdict() for r in restaurants]
     return pyjs.dumps(rest_list)
-
-
-
-
-
-
-
-
-
-
-
-
-
-#JSON APIs to view Restaurant Information
-# @json.route('/restaurant/<int:restaurant_id>/menu/JSON')
-# def restaurant_menu_json(restaurant_id):
-#     items = db.session.execute(text('select * from menu_item where restaurant_id = :restaurant_id'), {'restaurant_id': restaurant_id})
-#     items_list = [ i._asdict() for i in items ]
-#     return pyjs.dumps(items_list)
-
-
-# @json.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
-# def menu_item_json(restaurant_id, menu_id):
-#     menu_item = db.session.execute(text('select * from menu_item where id = :menu_id limit 1'), {'menu_id': menu_id})
-#     items_list = [ i._asdict() for i in menu_item ]
-#     return pyjs.dumps(items_list)
-
-# @json.route('/restaurant/JSON')
-# def restaurantsJSON():
-#     restaurants = db.session.execute(text('select * from restaurant'))
-#     rest_list = [ r._asdict() for r in restaurants ]
-#     return pyjs.dumps(rest_list)
